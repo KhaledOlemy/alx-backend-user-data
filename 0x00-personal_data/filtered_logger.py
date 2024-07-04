df021ac3cddd1acdf758f725c4b6523f95ff3d9a
khaledolemy <khaledolemy@gmail.com>
2024-07-05 01:49:13 +0300

    same output

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,8 +2,7 @@
 """Filtered logger, use regex to escape/ hide user data fields"""
 import re
 
-
 def filter_datum(fields, redaction, message, separator):
-    """Obfuscates specified fields in a log message."""
-    pattern = '|'.join(f'(?<={field}=)[^{separator}]+' for field in fields)
-    return re.sub(pattern, redaction, message)
+    """Obfuscates specified fields in a log message using regex."""
+    regex = fr"({'|'.join(fields)})=[^{separator}]+"
+    return re.sub(regex, lambda x: f"{x.group(1)}={redaction}", message)
