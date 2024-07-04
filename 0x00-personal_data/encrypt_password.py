5bbbdda60aef952f8834e09c956025028ab83581
khaledolemy <khaledolemy@gmail.com>
2024-07-05 02:05:13 +0300

    type annot

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -10,7 +10,7 @@ def hash_password(password: str) -> bytes:
     return hashed_password
 
 
-def is_valid(hashed_password, password):
+def is_valid(hashed_password: bytes, password: str) -> bool:
     """Validate that the provided password matches the hashed password."""
     password_bytes = password.encode('utf-8')
     return bcrypt.checkpw(password_bytes, hashed_password)
