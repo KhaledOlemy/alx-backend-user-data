d131d51da6717fc4dfb6418aa8c01a1bd35ff40f
khaledolemy <khaledolemy@gmail.com>
2024-07-05 01:37:37 +0300

    task 0

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Filtered logger, use regex to escape/ hide user data fields"""
+import re
+
+
+def filter_datum(fields, redaction, message, separator):
+    """Obfuscates specified fields in a log message."""
+    pattern = '|'.join(f'(?<={field}=)[^;]+' for field in fields)
+    return re.sub(pattern, redaction, message)
