7068426ea45c780915f69cf16dd4b66bf16fdcc9
khaledolemy <khaledolemy@gmail.com>
2024-07-05 02:04:41 +0300

    is password hash matching?

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -8,3 +8,9 @@ def hash_password(password: str) -> bytes:
     password_bytes = password.encode('utf-8')
     hashed_password = bcrypt.hashpw(password_bytes, bcrypt.gensalt())
     return hashed_password
+
+
+def is_valid(hashed_password, password):
+    """Validate that the provided password matches the hashed password."""
+    password_bytes = password.encode('utf-8')
+    return bcrypt.checkpw(password_bytes, hashed_password)
