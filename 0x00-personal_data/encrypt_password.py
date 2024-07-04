6efe45c691fbd735650fdd0adfdcbb475cd5b94d
khaledolemy <khaledolemy@gmail.com>
2024-07-05 01:58:11 +0300

    password secure

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Encrypting passwords, hasing to be stored in db"""
+import bcrypt
+
+
+def hash_password(password):
+    """Hash a password for storing."""
+    password_bytes = password.encode('utf-8')
+    hashed_password = bcrypt.hashpw(password_bytes, bcrypt.gensalt())
+    return hashed_password
