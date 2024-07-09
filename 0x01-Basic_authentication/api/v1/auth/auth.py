13474300a2d1c826b551cf945f726e9723de672e
khaledolemy <khaledolemy@gmail.com>
2024-07-10 02:00:13 +0300

    add excluded paths

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -15,7 +15,12 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """require authentication for path
         """
-        return False
+        if not path or not excluded_paths:
+            return True
+        path = path if path.endswith('/') else "{}/".format(path)
+        if path in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Get authorization header
