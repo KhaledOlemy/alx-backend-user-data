c197a386e759a4fca276e2aedac86734d23e903e
khaledolemy <khaledolemy@gmail.com>
2024-07-10 03:34:32 +0300

    base64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -12,4 +12,13 @@ class BasicAuth(Auth):
     """Basic authentication system
     decided by env vars
     """
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:  # noqa
+        """Returns only the base64 part
+        """
+        if not authorization_header:
+            return None
+        if type(authorization_header) is not str:
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header.split("Basic ")[-1]
