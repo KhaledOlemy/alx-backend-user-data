52d2c3284d266318d78b0f556105e4380360962f
khaledolemy <khaledolemy@gmail.com>
2024-07-12 01:32:26 +0300

    handle session IDs: user IDs

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python3
 """Session authentication mechanism"""
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """Session authentication class
     that inherits from Auth class
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a session based on the authenticated user"""
+        if not user_id or type(user_id) is not str:
+            return None
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
