b21fe33cadd71166022c6d3d364fc9c28ce591c5
khaledolemy <khaledolemy@gmail.com>
2024-07-12 01:15:24 +0300

    handle /user/me based on user authenticated

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -38,6 +38,7 @@ def handle_authorization() -> str:
         abort(401)
     if not auth.current_user(request):
         abort(403)
+    request.current_user = auth.current_user(request)
 
 
 @app.errorhandler(404)
